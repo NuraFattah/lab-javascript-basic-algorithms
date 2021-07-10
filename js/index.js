27953cc58bb65ed25c80d791ea9ebf546ef15580
“nura-fattah” <nura_fattah@outlook.de>
2021-07-10 16:56:43 +0200

    mostly finished

--- js/index.js
+++ js/index.js
@@ -1,5 +1,25 @@
 // Iteration 1: Names and Input
 
+let hacker1 = "The driver's name";
+let hacker2 = "The navigator's name";
+
+console.log(hacker1 + ' is XXXX');
+
+console.log(hacker2 + ' is YYYY');
+
+
+
+if ( hacker1 > hacker2){
+    console.log("The driver has the longest name, it has XX characters.");
+  } else if (hacker1 < hacker2){
+    console.log ("It seems that the navigator has the longest name, it has XX characters.");
+  } else {
+    console.log("Wow, you both have equally long names, XX characters!");
+  }
+  
+  
+  console.log(hacker2.toUpperCase());
+
 
 // Iteration 2: Conditionals
 
